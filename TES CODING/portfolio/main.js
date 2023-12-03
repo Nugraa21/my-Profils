fe93f09822084e579580939331c04fdfd5823bbb
Nugra21 <150422561+Nugraa21@users.noreply.github.com>
2023-12-03 18:32:49 +0700

    profils
    profils

---
+++ TES CODING/portfolio/main.js
@@ -0,0 +1,20 @@
+// Toggle & Responsive Navigation
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    // Toggle nav list and burger class
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
+
+// Clear form before unload
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
