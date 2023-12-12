2bcbb8c2b8449577fb261ee1621fa16911f1c4b9
Nugra21 <150422561+Nugraa21@users.noreply.github.com>
2023-12-12 16:33:14 +0700

    aaa
    aa

--- js/main.js
+++ js/main.js
@@ -18,3 +18,17 @@
 //     form.reset();
 //   }
 // };
+
+
+            window.addEventListener('scroll', function () {
+                var header = document.getElementById('navigasi');
+                var scrollPosition = window.scrollY;
+
+                if (scrollPosition > 100) {
+                    header.style.backgroundColor = '#60b4df';
+                    // header.style.backgroundColor = '#60b5df44'; // sebelum
+                } else {
+                     header.style.backgroundColor = ' #00000000';
+                    // header.style.backgroundColor = '#60b4df'; // sesudah 
+                }
+            });
