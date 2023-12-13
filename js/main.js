666301180076e43e7e8b9453100c2d9ecf0b68da
Nugra21 <150422561+Nugraa21@users.noreply.github.com>
2023-12-13 08:50:56 +0700

    r
    r

--- js/main.js
+++ js/main.js
@@ -18,17 +18,40 @@
 //     form.reset();
 //   }
 // };
+document.addEventListener("DOMContentLoaded", function () {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  window.addEventListener("scroll", function () {
+    // Tampilkan tombol ketika pengguna menggulir ke bawah
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
+
+  // Atur scroll ke atas saat tombol diklik
+  scrollToTopBtn.addEventListener("click", function () {
+    document.body.scrollTop = 0; // Untuk browser Safari
+    document.documentElement.scrollTop = 0; // Untuk browser lainnya
+  });
+});
+
 
 
             window.addEventListener('scroll', function () {
                 var header = document.getElementById('navigasi');
                 var scrollPosition = window.scrollY;
 
-                if (scrollPosition > 100) {
-                    header.style.backgroundColor = '#60b4df';
+                if (scrollPosition > 150) {
+                    header.style.backgroundColor = ' #00aeff';
                     // header.style.backgroundColor = '#60b5df44'; // sebelum
                 } else {
                      header.style.backgroundColor = ' #00000000';
                     // header.style.backgroundColor = '#60b4df'; // sesudah 
                 }
             });
+
+
+
+
