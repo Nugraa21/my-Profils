72fec984391faa9f215759d0955d4b9a493cf1dc
Nugra21 <150422561+Nugraa21@users.noreply.github.com>
2024-01-17 19:43:16 +0700

    a

--- js/main.js
+++ js/main.js
@@ -80,26 +80,26 @@ document.addEventListener("DOMContentLoaded", function () {
 // -------------------------------- Mematikan mode inspek atau klick kanan -------------------------------- //
 
 
-// document.addEventListener('contextmenu', function (e) {
-//   e.preventDefault();
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
 
-//   var confirmation = confirm('Menu konteks dinonaktifkan! Klik "OK" untuk menghubungi kami melalui GitHub atau WhatsApp.');
+  var confirmation = confirm('Menu konteks dinonaktifkan! Klik "OK" untuk menghubungi kami melalui GitHub atau WhatsApp.');
 
-//   if (confirmation) {
-//     var githubLink = 'https://github.com/Nugraa21';
-//     var whatsappLink = 'https://wa.me/your-whatsapp';
+  if (confirmation) {
+    var githubLink = 'https://github.com/Nugraa21';
+    var whatsappLink = 'https://wa.me/your-whatsapp';
 
-//     var userChoice = prompt('Pilih salah satu:\n1. GitHub\n2. WhatsApp');
+    var userChoice = prompt('Pilih salah satu:\n1. GitHub\n2. WhatsApp');
 
-//     if (userChoice === '1') {
-//       window.location.href = githubLink;
-//     } else if (userChoice === '2') {
-//       window.location.href = whatsappLink;
-//     } else {
-//       alert('Pilihan tidak valid.');
-//     }
-//   }
-// });
+    if (userChoice === '1') {
+      window.location.href = githubLink;
+    } else if (userChoice === '2') {
+      window.location.href = whatsappLink;
+    } else {
+      alert('Pilihan tidak valid.');
+    }
+  }
+});
 
 // -------------------------------- Mencegah agar tidak bisa di copy -------------------------------- //
 
